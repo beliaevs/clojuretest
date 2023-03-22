76ba225c3f53a6ba0e7956cbb7510c5867804db7
Alexander Beliaev <beliaevs@gmail.com>
2023-03-23 00:48:13 +0300

    formatting

--- factorial.clj
+++ factorial.clj
@@ -1,3 +1,8 @@
-(defn factorial [n] (if (= n 0) 1 (* (bigint n) ( factorial (- n 1) ) ) ) )
-(let [a 50] (println "factorial(" a ") =" (factorial a) ) )
+(defn factorial [n] 
+  (if (= n 0) 
+    1 
+    (* (bigint n) (factorial (- n 1) ) ) ) )
+
+(let [a 50] 
+  (println "factorial(" a ") =" (factorial a) ) )
 
--- hello.clj
+++ hello.clj
@@ -1,2 +1,19 @@
 (println "hello!") ; say hello
+; check recursion depth
+(defn f [n] 
+  (if (= 0 n) 
+    0 
+    (+ n (f (- n 1) ) ) ) )
 
+(let [a 1000] 
+  (println (f a) ) ) ;no tail recursion
+
+(defn loopf [n] 
+  (loop [cnt n 
+         acc 0]
+    (if (= cnt 0) 
+      acc 
+      (recur (- cnt 1) (+ acc cnt) ) ) ) )
+
+(let [a 10000] 
+  (println (loopf a) ) ) ;special loop
