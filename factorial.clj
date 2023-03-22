e6cb989749236cebf700b3ce89c6d4511a527fa4
Alexander Beliaev <beliaevs@gmail.com>
2023-03-22 22:40:17 +0300

    recursive factorial

--- factorial.clj
+++ factorial.clj
@@ -1,3 +1,3 @@
 (defn factorial [n] (if (= n 0) 1 (* (bigint n) ( factorial (- n 1) ) ) ) )
-(let [a 50] (println "factorial(" a ")=" (factorial a) ) )
+(let [a 50] (println "factorial(" a ") =" (factorial a) ) )
 
